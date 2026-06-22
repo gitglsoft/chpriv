f93a351298a140526d0cb3a77a912817583fe019
luciano <glsoft@inwind.it>
2026-06-22 10:08:31 +0200

    Remove startup event listeners and import Firebase functions

--- app.js
+++ app.js
@@ -1,37 +1,5 @@
-console.log("ChPriv avviato");
-
-const startup = document.getElementById("startup");
-const chatContainer = document.getElementById("chatContainer");
-
-const createBtn = document.getElementById("btnCreateRoom");
-const joinBtn = document.getElementById("btnJoinRoom");
-
-createBtn.addEventListener("click", () => {
-
-  const nickname =
-    document.getElementById("nickname").value.trim();
-
-  const password =
-    document.getElementById("roomPassword").value.trim();
-
-  if(!nickname){
-    alert("Inserisci un nickname");
-    return;
-  }
-
-  if(!password){
-    alert("Inserisci una password stanza");
-    return;
-  }
-
-  alert(
-    "Firebase collegato correttamente.\n\n" +
-    "Nickname: " + nickname +
-    "\nPassword: " + password
-  );
-
-});
-
-joinBtn.addEventListener("click", () => {
-  alert("Funzione ENTRA in costruzione");
-});
+import {
+  collection,
+  addDoc,
+  serverTimestamp
+}
