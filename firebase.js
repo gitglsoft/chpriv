3fab5cb7a831f8469da1844f123d1bfdf02f268a
luciano <glsoft@inwind.it>
2026-06-24 17:54:51 +0200

    Add onDisconnect to Firebase database functions

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
-import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
+import { getDatabase, ref, set, get, onValue, onDisconnect } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCg-Q8z5ihQxuDtQpTnaq9zYmUwI5on6lg",
@@ -15,15 +15,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Mantieni solo questa riga per rtdb:
 export const rtdb = getDatabase(app, "https://chpriv-8a3da-default-rtdb.europe-west1.firebasedatabase.app");
 
-// Inizializzazione sicura
 export async function initFirebase() {
   await signInAnonymously(auth);
   console.log("Firebase inizializzato correttamente");
 }
 
-// Esponiamo le funzioni per app.js
-window.chpriv = { db, rtdb, ref, set, get, onValue };
+// Esponiamo le funzioni necessarie per app.js
+window.chpriv = { db, rtdb, ref, set, get, onValue, onDisconnect };
